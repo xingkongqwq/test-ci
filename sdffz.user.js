213d206c2d6dcfdb09a9bb8fc483a9324ed5c303
xingkongqwq <xkawa@outlook.com>
2022-10-04 15:38:04 +0800

    Update sdffz.user.js

--- sdffz.user.js
+++ sdffz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         闪电发辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  闪电发辅助
 // @author       xingkongqwq
 // @match        *://fdc.shandjj.com/*
